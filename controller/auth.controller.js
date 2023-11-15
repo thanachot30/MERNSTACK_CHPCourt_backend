c4de2919212c080e1428f58f762e06d232491775
thanachot30 <su.thanachot@gmail.com>
2023-11-15 11:40:01 +0700

    error handle

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -1,13 +1,14 @@
 import User from '../models/user.model.js'
 
-export const signup = async (req,res)=>{
+
+export const signup = async (req,res,next)=>{
     const {username,email,password} = req.body;
     const newUser = new User({username,email,password});
     try{
         await newUser.save();
         res.status(200).json('User create successfully!');
     }catch(error){
-        res.status(500).json(error.message);
+        next(error);
     }
     
 }   
--- index.js
+++ index.js
@@ -11,8 +11,19 @@ import authRouter from "./routes/auth.route.js"
 
 
 app.use(express.json());
+
 app.use('/api/user',userRouter);
 app.use('/api/auth',authRouter);
+
+app.use((err,req,res,next)=>{
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+        success:false,
+        statusCode,
+        message
+    });
+})
 
 
 mongoose.connect(process.env.MONGO)
